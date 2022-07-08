153e866696721f9d21884bb7b24bd19bbdc008c0
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-07-08 11:55:13 +0300

    created responsive images

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import { debounce } from './utils/throttle/debounce';
 import { FluidTypography } from './structures/FluidTypography';
+import { ResponsiveImage } from './structures/ResponsiveImage';
 
 const listening = debounce(() => {
 	const fluidTypography = new FluidTypography();
 	fluidTypography.onMounted();
 	fluidTypography.onResize();
+
+	const responsiveImage = new ResponsiveImage();
+	responsiveImage.onMounted();
 
 	console.log('App JS listening...');
 }, 0, false);

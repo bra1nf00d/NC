1f873118ccb26b971eb33abba04dbd3012f059d3
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-07-12 17:25:14 +0300

    created remove action for cart button cross

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import { debounce } from './utils/throttle/debounce';
 import { FluidTypography } from './structures/FluidTypography';
 import { ResponsiveImage } from './structures/ResponsiveImage';
 import { ButtonAmount } from './structures/ui/ButtonAmount';
+import { ButtonCross } from './structures/ui/ButtonCross';
 import { Cutter } from './utils/enums/cutter.enums';
 
 const listening = debounce(() => {
@@ -14,6 +15,11 @@ const listening = debounce(() => {
 
 	const buttonAmount = new ButtonAmount();
 	buttonAmount.onMounted();
+
+	const cartButtonCross = new ButtonCross('.cart .button-cross');
+	cartButtonCross.onClick((event) => {
+		event.currentTarget.closest('.cart__column').remove();
+	});
 
 	const cutterProductTitle = new Cutter('.product-title');
 	cutterProductTitle.cut(36);
